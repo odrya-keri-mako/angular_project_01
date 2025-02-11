14b356d3fa7d4ad20b7fda3ace7ed5dbfeb15e95
odrya-keri-mako <odry.attila@keri.mako.hu>
2025-02-11 11:20:07 +0100

    Remove develop message

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,6 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
+import { enableProdMode, isDevMode } from "@angular/core";
+
+if (isDevMode()) {
+  enableProdMode();
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
